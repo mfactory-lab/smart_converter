a2c94424d89e029497bc9060caef66a651f2bf5f
Illia Fedotov <illia.fedotov@litslink.com>
2023-04-18 18:47:00 +0300

    feat(program): add instruction for adding new pair

---
+++ programs/omnisol/src/instructions/add_pair.rs
@@ -0,0 +1,58 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token;
+
+use crate::{state::{Manager, Admin, Pair}, ErrorCode};
+
+/// The manager can add new pair.
+pub fn handle(ctx: Context<AddPair>) -> Result<()> {
+    let manager = &mut ctx.accounts.manager;
+    let pair = &mut ctx.accounts.pair;
+    let pair_authority = ctx.accounts.pair_authority.key;
+    let token_a = &ctx.accounts.token_a;
+    let token_b = &ctx.accounts.token_b;
+
+    if token_b.mint_authority.is_none() || token_b.mint_authority.unwrap() != *pair_authority {
+        msg!("Invalid utility token mint authority");
+        return Err(ErrorCode::Unauthorized.into());
+    }
+
+    pair.manager_wallet = manager.authority;
+    pair.token_a = token_a.key();
+    pair.token_b = token_b.key();
+
+    Ok(())
+}
+
+#[derive(Accounts)]
+pub struct AddPair<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [Manager::SEED, authority.key().as_ref()],
+        bump,
+    )]
+    pub manager: Box<Account<'info, Manager>>,
+
+    #[account(
+        init,
+        seeds = [Pair::SEED, token_a.key().as_ref(), token_b.key().as_ref()],
+        bump,
+        payer = authority,
+        space = Pair::SIZE
+    )]
+    pub pair: Box<Account<'info, Pair>>,
+
+    /// CHECK: no needs to check, only for signing
+    #[account(seeds = [pair.key().as_ref()], bump)]
+    pub pair_authority: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub token_a: Account<'info, token::Mint>,
+
+    #[account(mut)]
+    pub token_b: Account<'info, token::Mint>,
+
+    pub system_program: Program<'info, System>,
+}
--- programs/omnisol/src/instructions/mod.rs
+++ programs/omnisol/src/instructions/mod.rs
@@ -2,8 +2,12 @@ pub mod add_manager;
 pub mod update_manager;
 pub mod set_admin;
 pub mod change_platform_state;
+pub mod add_pair;
+pub mod pause_pair;
 
 pub use add_manager::*;
 pub use update_manager::*;
 pub use set_admin::*;
 pub use change_platform_state::*;
+pub use add_pair::*;
+pub use pause_pair::*;
