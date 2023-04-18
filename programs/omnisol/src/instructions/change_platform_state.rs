1bbaff4f2326f2e01619bcc51f1a70acbd972805
Illia Fedotov <illia.fedotov@litslink.com>
2023-04-18 18:11:43 +0300

    feat(program): add instructions for changing platform's state

---
+++ programs/omnisol/src/instructions/change_platform_state.rs
@@ -0,0 +1,45 @@
+use anchor_lang::prelude::*;
+
+use crate::{state::Admin, ErrorCode};
+
+/// The admin can pause platform.
+pub fn pause(ctx: Context<ChangePlatformState>) -> Result<()> {
+    let admin = &mut ctx.accounts.admin;
+
+    if admin.is_platform_paused {
+        return Err(ErrorCode::AlreadyPaused.into());
+    }
+
+    admin.is_platform_paused = true;
+
+    Ok(())
+}
+
+/// The admin can resume platform.
+pub fn resume(ctx: Context<ChangePlatformState>) -> Result<()> {
+    let admin = &mut ctx.accounts.admin;
+
+    if !admin.is_platform_paused {
+        return Err(ErrorCode::AlreadyResumed.into());
+    }
+
+    admin.is_platform_paused = false;
+
+    Ok(())
+}
+
+#[derive(Accounts)]
+pub struct ChangePlatformState<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [Admin::SEED],
+        bump,
+        has_one = authority,
+    )]
+    pub admin: Box<Account<'info, Admin>>,
+
+    pub system_program: Program<'info, System>,
+}
--- programs/omnisol/src/instructions/mod.rs
+++ programs/omnisol/src/instructions/mod.rs
@@ -1,5 +1,9 @@
 pub mod add_manager;
 pub mod update_manager;
+pub mod set_admin;
+pub mod change_platform_state;
 
 pub use add_manager::*;
 pub use update_manager::*;
+pub use set_admin::*;
+pub use change_platform_state::*;
